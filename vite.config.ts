51cd88504ed8ddec34854b36b7f84488922a4b59
Hamza Faidi <hamzafa7d7@gmail.com>
2025-12-11 21:18:50 +0100

    fixed repo name

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/awesome-low-level-design/'
+  base: '/SystemDesignForInterviewsTrainingQuizApp/'
 })
