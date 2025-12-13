32d9d59432e3f5148a08fccbf0d5086049b9a149
Hamza Faidi <hamzafa7d7@gmail.com>
2025-12-13 14:01:10 +0100

    fixed the router issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 import { AppLayout } from './components/AppLayout'
 import { AboutPage } from './pages/AboutPage'
 import { ChapterPage } from './pages/ChapterPage'
 import { HomePage } from './pages/HomePage'
 import { NotFoundPage } from './pages/NotFoundPage'
 
-const basePath = import.meta.env.BASE_URL ?? '/'
-
 function App() {
   return (
-    <BrowserRouter basename={basePath}>
+    <HashRouter>
       <AppLayout>
         <Routes>
           <Route path="/" element={<HomePage />} />
@@ -19,7 +17,7 @@ function App() {
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </AppLayout>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
